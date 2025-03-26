67c11abcc5df9a6501976fd1cd213c7a713a1c76
Ilhan Ghauri <ilhanghauri@Ilhans-MacBook-Air.local>
2025-03-26 15:21:06 +0100

    Bon App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
-import Footer from './components/footer.jsx'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Lifestyle from "./pages/Lifestyle";
+import Football from "./pages/Football";
+import Header from "./components/Header";
+import Footer from "./components/footer";
+import HomePage from "./HomePage"
+
 
 function App() {
     return (
-        <>
-            <Footer />
-        </>
-    )
+        <Router>
+            <Header/>
+            <Routes>
+                  <Route path="/lifestyle" element={<Lifestyle />} />
+                  <Route path="/football" element={<Football />} />
+            </Routes>
+            <HomePage/>
+            <Footer/>
+        </Router>
+    );
 }
 
-export default App
+export default App;
