95c547cf83d1c019fe88a1756e7b64df8fd70b7d
MonsOnc61 <zhongyuye2001@gmail.com>
2025-04-06 18:28:29 +0200

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,17 @@ import Footer from "./footer.jsx";
 const App = () => {
   return (
     <Router>
+      <div className="flex flex-col min-h-screen">
         <Header />
-        <Routes>
-          <Route path="/" element={<PageAccueil />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/planning" element={<Planning />} />
-        </Routes>
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/" element={<PageAccueil />} />
+              <Route path="/menu" element={<Menu />} />
+              <Route path="/planning" element={<Planning />} />
+            </Routes>
+          </div>
         <Footer />
+      </div>
     </Router>
   );
 };
