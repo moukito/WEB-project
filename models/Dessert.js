7e9e6c45910b1fd8094a729b6b4e5177eed4889d
moukito <theofremont50@gmail.com>
2025-04-07 07:45:55 +0200

    Add models for Appetizer, MainDish, Dessert, and Menu with printing functionality

---
+++ models/Dessert.js
@@ -0,0 +1,15 @@
+import Dish from "models/Dish.js";
+
+class Dessert extends Dish {
+    constructor(name, description, cost, ingredients = [], containLactose = true) {
+        super(name, description, cost, ingredients);
+        this.containLactose = containLactose;
+    }
+
+    print() {
+        let base = super.print();
+        return `Dessert: ${base}${this.containLactose ? '' : ' (sans lactose)'}`;
+    }
+}
+
+export default Dessert;
