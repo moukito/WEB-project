91540aeddd4872da6dff2925bb1ef4a88e612649
moukito <theofremont50@gmail.com>
2025-04-07 07:46:11 +0200

    Add models for Appetizer, MainDish, Dessert, and Menu with print functionality

---
+++ models/Appetizer.js
@@ -0,0 +1,15 @@
+import Dish from "models/Dish.js";
+
+class Appetizer extends Dish {
+    constructor(name, description, cost = 0, ingredients = [], isCold = false) {
+        super(name, description, cost, ingredients);
+        this.isCold = isCold;
+    }
+
+    print() {
+        let base = super.print();
+        return `Entrée: ${base}${this.isCold ? ' (servi froid)' : ''}`;
+    }
+}
+
+export default Appetizer;
